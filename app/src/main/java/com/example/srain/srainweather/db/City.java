1afcf1c6628ac3ce71043eb9884e259cae0a0b28
Srain <1303467073@qq.com>
2018-04-30 16:06:37 +0800

    First commit

---
+++ app/src/main/java/com/example/srain/srainweather/db/City.java
@@ -0,0 +1,12 @@
+package com.example.srain.srainweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Project: SrainWeather
+ * Date: 2018/4/30
+ *
+ * @author srain
+ */
+public class City extends DataSupport {
+}
