d1ad725c1e9d28690c7d84c138a5c7014cab892c
Srain <1303467073@qq.com>
2018-04-30 16:29:47 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/example/srain/srainweather/db/Province.java
+++ app/src/main/java/com/example/srain/srainweather/db/Province.java
@@ -9,4 +9,34 @@ import org.litepal.crud.DataSupport;
  * @author srain
  */
 public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceCode;
+
+    private int provinceName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(int provinceName) {
+        this.provinceName = provinceName;
+    }
 }
